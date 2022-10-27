a12ed643deba11ed81b6cad1e4ce8285ca031036
Andreas Rissling <a.rissling@steinberg.de>
2022-10-27 16:49:51 +0200

    Fix warning in jest about compiler

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,16 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
 };
 
 export default config;
