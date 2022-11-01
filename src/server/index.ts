2f26d7f57c189d53d494a076b747bf889563f393
Andreas Rissling <a.rissling@steinberg.de>
2022-11-01 09:41:21 +0100

    Add logic to return student as json

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,21 @@
 import * as express from 'express';
+import { Student } from '../types/index';
 
 const app = express();
 const port = 8000;
 
+const student: Student = {
+  firstName: 'Max',
+  surname: 'Mustermann',
+  gender: 'male',
+  fieldOfStudy: 'informatics',
+  birthday: new Date(1993, 1, 3),
+};
+
 app.get('/', (req, res) => {
-  res.type('text/html');
+  res.type('application/json');
   res.status(200);
-  res.send('Hello World!');
+  res.send(student);
 });
 
 app.listen(port, () => {
