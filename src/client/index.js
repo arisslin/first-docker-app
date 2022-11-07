9eea6ecb0c1f1f1bfa1ce6dfd394258d7aa87f08
Andreas Rissling <a.rissling@steinberg.de>
2022-11-07 10:27:04 +0100

    Create logic to fetch students data

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,17 @@
 const serverUrl = 'http://localhost';
 const port = '8000';
 
-const students = await fetch(serverUrl + ':' + port + '/students');
+fetchStudents()
+  .then((studentsData) => console.log(studentsData))
+  .catch((error) => console.error(error));
 
-console.log(students);
+async function fetchStudents() {
+  const response = await fetch(serverUrl + ':' + port + '/students');
+
+  if (!response.ok) {
+    const message = `An error has occured. Status ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+}
