df800f85880bee75b29024b3897016a77f407d07
Andreas Rissling <a.rissling@steinberg.de>
2022-11-08 11:42:29 +0100

    feat(client): Fetch todos instead of students

--- src/helpers/fetchHelpers.ts
+++ src/helpers/fetchHelpers.ts
@@ -1,7 +1,7 @@
 import { serverUrl, serverPort } from '../constans/index';
 
-export async function fetchStudents() {
-  const response = await fetch(serverUrl + ':' + serverPort + '/students');
+export async function fetchToDos() {
+  const response = await fetch(serverUrl + ':' + serverPort + '/todos');
 
   if (!response.ok) {
     const message = `An error has occured. Status ${response.status}`;
