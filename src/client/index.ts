29e3056076a6547dfadf94e976baebdacaa8d0d8
Andreas Rissling <a.rissling@steinberg.de>
2022-11-10 14:53:52 +0100

    feat(client): Create logic to render todos

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,9 +5,38 @@ import { serverPort, serverUrl } from '../constans/index';
 const toDosURL = `${serverUrl}:${serverPort}/todos`;
 
 fetchFrom<ToDo[]>(toDosURL)
-  .then((toDos) => renderToDosTable(toDos))
+  .then((toDos) => renderToDos(toDos))
   .catch((error) => console.error(error));
 
-function renderToDosTable(toDos: ToDo[]): void {
-  console.log(toDos);
+function renderToDos(toDos: ToDo[]): void {
+  if (toDos.length) {
+    const mainElement = document.querySelector('main');
+    const toDosOverview = document.createElement('fieldset');
+    const legend = document.createElement('legend');
+
+    legend.innerText = 'To dos:';
+    toDosOverview.appendChild(legend);
+    toDos.forEach((toDo) => {
+      const id = `to-do-${toDo.id.toString()}`;
+      const checkbox = document.createElement('input');
+
+      checkbox.type = 'checkbox';
+      checkbox.id = id;
+      checkbox.name = id;
+      checkbox.checked = toDo.isDone;
+
+      const label = document.createElement('label');
+
+      label.htmlFor = id;
+      label.innerText = toDo.task;
+
+      const div = document.createElement('div');
+
+      div.appendChild(checkbox);
+      div.appendChild(label);
+      toDosOverview.appendChild(div);
+    });
+
+    mainElement?.appendChild(toDosOverview);
+  }
 }
