1c29d44fe626bae3c4b5a6121420d1804e3c2946
Andreas Rissling <a.rissling@steinberg.de>
2022-11-10 15:06:47 +0100

    refactor(client): Adjust logic to generate to dos overview

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,38 +5,43 @@ import { serverPort, serverUrl } from '../constans/index';
 const toDosURL = `${serverUrl}:${serverPort}/todos`;
 
 fetchFrom<ToDo[]>(toDosURL)
-  .then((toDos) => renderToDos(toDos))
+  .then((toDos) => {
+    const toDosOverview = renderToDos(toDos);
+
+    toDosOverview && document.querySelector('main')?.appendChild(toDosOverview);
+  })
   .catch((error) => console.error(error));
 
-function renderToDos(toDos: ToDo[]): void {
-  if (toDos.length) {
-    const mainElement = document.querySelector('main');
-    const toDosOverview = document.createElement('fieldset');
-    const legend = document.createElement('legend');
-
-    legend.innerText = 'To dos:';
-    toDosOverview.appendChild(legend);
-    toDos.forEach((toDo) => {
-      const id = `to-do-${toDo.id.toString()}`;
-      const checkbox = document.createElement('input');
-
-      checkbox.type = 'checkbox';
-      checkbox.id = id;
-      checkbox.name = id;
-      checkbox.checked = toDo.isDone;
-
-      const label = document.createElement('label');
-
-      label.htmlFor = id;
-      label.innerText = toDo.task;
-
-      const div = document.createElement('div');
-
-      div.appendChild(checkbox);
-      div.appendChild(label);
-      toDosOverview.appendChild(div);
-    });
-
-    mainElement?.appendChild(toDosOverview);
+function renderToDos(toDos: ToDo[]): HTMLFieldSetElement | undefined {
+  if (!toDos.length) {
+    return undefined;
   }
+
+  const fieldSet = document.createElement('fieldset');
+  const legend = document.createElement('legend');
+
+  legend.innerText = 'To dos:';
+  fieldSet.appendChild(legend);
+  toDos.forEach((toDo) => {
+    const id = `to-do-${toDo.id.toString()}`;
+    const checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.id = id;
+    checkbox.name = id;
+    checkbox.checked = toDo.isDone;
+
+    const label = document.createElement('label');
+
+    label.htmlFor = id;
+    label.innerText = toDo.task;
+
+    const div = document.createElement('div');
+
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    fieldSet.appendChild(div);
+  });
+
+  return fieldSet;
 }
