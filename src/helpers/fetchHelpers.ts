116999a82fd5a03010bf21ce03574a084d1c6eaf
Andreas Rissling <a.rissling@steinberg.de>
2022-11-10 10:40:08 +0100

    test(fetchFrom): It returns a fetched text

--- src/helpers/fetchHelpers.ts
+++ src/helpers/fetchHelpers.ts
@@ -1,10 +1,13 @@
-export async function fetchFrom<Type>(url: string): Promise<Type> {
+export async function fetchFrom<Type>(
+  url: string,
+  responseType: 'json' | 'text' = 'json'
+): Promise<Type> {
   try {
     const response = await fetch(url, {
       method: 'GET',
     });
 
-    return await response.json();
+    return (await responseType) === 'json' ? response.json() : response.text();
   } catch (error) {
     console.error('Fetch failed!');
 
