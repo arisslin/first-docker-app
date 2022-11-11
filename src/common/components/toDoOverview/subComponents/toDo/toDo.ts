80b44df2de8f773bdfe5823a2ee7741158e57486
Andreas Rissling <a.rissling@steinberg.de>
2022-11-11 12:17:13 +0100

    feat(toDo): Style element

--- src/common/components/toDoOverview/subComponents/toDo/toDo.ts
+++ src/common/components/toDoOverview/subComponents/toDo/toDo.ts
@@ -1,21 +1,25 @@
 import { ToDo } from '../../../../types/index';
+import './toDo.css';
 
 function createToDo({ id, task, isDone }: ToDo): HTMLDivElement {
   const identifier = 'to-do-' + id;
   const checkbox = document.createElement('input');
 
-  checkbox.type = 'checkbox';
+  checkbox.className = 'to-do__checkbox';
+  checkbox.checked = isDone;
   checkbox.id = identifier;
   checkbox.name = identifier;
-  checkbox.checked = isDone;
+  checkbox.type = 'checkbox';
 
   const label = document.createElement('label');
 
+  label.className = 'to-do__label';
   label.htmlFor = identifier;
   label.innerText = task;
 
   const div = document.createElement('div');
 
+  div.className = 'to-do';
   div.appendChild(checkbox);
   div.appendChild(label);
 
--- src/common/components/toDoOverview/toDoOverview.ts
+++ src/common/components/toDoOverview/toDoOverview.ts
@@ -1,15 +1,19 @@
 import { ToDo } from '../../types/index';
 import createToDo from './subComponents/toDo/toDo';
+import './toDoOverview.css';
 
 function createToDoOverview(toDos: ToDo[]): HTMLFieldSetElement | undefined {
   if (!toDos.length) {
     return undefined;
   }
 
-  const fieldSet = document.createElement('fieldset');
   const legend = document.createElement('legend');
 
   legend.innerText = 'To dos:';
+
+  const fieldSet = document.createElement('fieldset');
+
+  fieldSet.className = 'to-do-overview';
   fieldSet.appendChild(legend);
   toDos.forEach((toDo) => {
     const toDoElement = createToDo(toDo);
