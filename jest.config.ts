f7898eba4357a245b5067a7868444b8f9f2540dd
Andreas Rissling <a.rissling@steinberg.de>
2022-11-11 15:36:21 +0100

    config: Mock css styles for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/src/common/mocks/styleMock.ts',
+  },
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
