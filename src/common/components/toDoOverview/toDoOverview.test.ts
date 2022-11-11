17a03d458f9821fb5dcfec258fc3a77e70904373
Andreas Rissling <a.rissling@steinberg.de>
2022-11-11 11:20:38 +0100

    test(createToDoOverview): It renders as fieldset

--- src/common/components/toDoOverview/toDoOverview.test.ts
+++ src/common/components/toDoOverview/toDoOverview.test.ts
@@ -1,7 +1,14 @@
+import { mockedToDos } from '../../mocks/mockedTodos';
 import createToDoOverview from './toDoOverview';
 
 describe('createToDoOverview()', () => {
+  const element = createToDoOverview(mockedToDos);
+
   it('returns undefined if there are no to dos', () => {
     expect(createToDoOverview([])).toBe(undefined);
+  });
+
+  it('renders as HTMLFieldSetElement', () => {
+    expect(element?.tagName).toBe('FIELDSET');
   });
 });
