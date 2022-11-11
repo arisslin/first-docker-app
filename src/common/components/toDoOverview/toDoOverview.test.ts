15ea34472fbe64339bf4c92767d6a07d094aac2f
Andreas Rissling <a.rissling@steinberg.de>
2022-11-11 11:32:53 +0100

    test(createToDoOverview): It creates all provided to dos

--- src/common/components/toDoOverview/toDoOverview.test.ts
+++ src/common/components/toDoOverview/toDoOverview.test.ts
@@ -8,7 +8,16 @@ describe('createToDoOverview()', () => {
     expect(createToDoOverview([])).toBe(undefined);
   });
 
-  it('renders as HTMLFieldSetElement', () => {
+  it('gets created as HTMLFieldSetElement', () => {
     expect(element?.tagName).toBe('FIELDSET');
+  });
+
+  it('creates all provided to dos', () => {
+    mockedToDos.forEach((toDo) => {
+      const identifier = 'to-do-' + toDo.id;
+
+      expect(element?.querySelector(`input[name=${identifier}]`)).toBeTruthy();
+      expect(element?.querySelector(`label[for=${identifier}]`)).toBeTruthy();
+    });
   });
 });
