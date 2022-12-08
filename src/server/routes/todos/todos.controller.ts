60846a5efb25b140ba0b5ed5ca4d3b4c2251c6cd
Andreas Rissling <a.rissling@steinberg.de>
2022-12-08 12:22:39 +0100

    refactor(server): Make code more readable

--- src/server/routes/todos/todos.controller.ts
+++ src/server/routes/todos/todos.controller.ts
@@ -1,25 +1,21 @@
 import { Response, Request } from 'express';
-import { readFile } from 'fs';
+import { readJSONFromFS } from '../../../common/helpers/fsHelpers';
+import { ToDo } from '../../../common/types/index';
 
 const dataPath = 'src/common/mocks/mockedTodos.json';
 
-export function getToDos(request: Request, response: Response): void {
-  readFile(dataPath, (error, data) => {
-    if (error) {
-      console.error(error);
-    } else {
-      try {
-        const dataString = data.toString();
-        const dataJSON = JSON.parse(dataString);
+export async function getToDos(request: Request, response: Response) {
+  try {
+    const toDos = await readJSONFromFS<ToDo[]>(dataPath);
 
-        response.type('application/json');
-        response.status(200);
-        response.send(dataJSON);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  });
+    console.log(toDos);
+
+    response.type('application/json');
+    response.status(200);
+    response.send(toDos);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export function postToDos(request: Request, response: Response): void {
