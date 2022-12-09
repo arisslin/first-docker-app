31022f96d0dfc9b6711fdcb209372a5b74ac1a75
Andreas Rissling <a.rissling@steinberg.de>
2022-12-09 13:16:13 +0100

    feat(server): Handle errors for getting to dos

--- src/server/routes/todos/todos.controller.ts
+++ src/server/routes/todos/todos.controller.ts
@@ -2,19 +2,18 @@ import { Response, Request } from 'express';
 import { readJSONFromFS } from '../../../common/helpers/fsHelpers';
 import { ToDo } from '../../../common/types/index';
 
-const dataPath = 'src/common/mocks/mockedTodos.json';
+const dataPath = 'src/common/mocks/mockedTodosa.json';
 
 export async function getToDos(request: Request, response: Response) {
   try {
     const toDos = await readJSONFromFS<ToDo[]>(dataPath);
 
-    console.log(toDos);
-
     response.type('application/json');
     response.status(200);
     response.send(toDos);
   } catch (error) {
-    console.error(error);
+    console.error(getToDos.name, '-', error);
+    response.sendStatus(500);
   }
 }
 
