32a58b4cc890d6c59c95e8eece1d85abecd3bb95
Andreas Rissling <a.rissling@steinberg.de>
2023-01-20 11:19:34 +0100

    feat: Add delete icon to to-do

--- src/common/components/toDoOverview/subComponents/toDo/toDo.ts
+++ src/common/components/toDoOverview/subComponents/toDo/toDo.ts
@@ -17,11 +17,24 @@ function createToDo({ id, task, isDone }: ToDo): HTMLDivElement {
   label.htmlFor = identifier;
   label.innerText = task;
 
+  const trashIcon = document.createElement('i');
+
+  trashIcon.className = 'fa-solid fa-trash';
+
+  const deleteButton = document.createElement('button');
+
+  deleteButton.className = 'to-do__delete-button';
+  deleteButton.appendChild(trashIcon);
+  deleteButton.addEventListener('click', () => {
+    console.log('Delete');
+  });
+
   const div = document.createElement('div');
 
   div.className = 'to-do';
   div.appendChild(checkbox);
   div.appendChild(label);
+  div.appendChild(deleteButton);
 
   return div;
 }
